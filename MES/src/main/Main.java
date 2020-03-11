a64431a68b3af5ec9aa005fe4390af170d0a22a7
fábio <fabiouds@hotmail.com>
2020-03-11 16:29:02 +0000

    update

--- MES/src/main/Main.java
+++ MES/src/main/Main.java
@@ -1,12 +1,37 @@
 package main;
 
-import db.DataBase;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+
+import opc.OPCUA_Connection;
+import xml.Xml;
 
 public class Main {
-
+	public static OPCUA_Connection MyConnection ;
+	public static OpcUaClient client;
+	public static String aux = "teste";
+	public static String Client = "opc.tcp://"+aux+":53880";
 	public static void main(String[] args) {
-		DataBase db = DataBase.getInstance();
-		System.out.println(db.executeQuery("INSERT into fabrica.fabrica (npecasfabrica) VALUES (2)"));
+		Xml xml = new Xml();
+		xml.read();
+		
+		MyConnection = new OPCUA_Connection(Client);
+		MyConnection.MakeConnection();
+		
+		
+		//Inicialização das variáveis
+		String cellName, variable;
+		Boolean value;
+		cellName = "eu";
+		variable = "Hello";
+		value= false;
+		
+		/*Funções: get_Value para saber o valor de uma variavel
+		*          setValue para escrever o valor numa variavel
+		*/
+		System.out.println("--------------Value Get--------------");
+		MyConnection.get_Value(cellName,variable);
+		System.out.println("--------------Value Change--------------");
+		MyConnection.setValue(cellName,variable,value);
 	}
 
 }

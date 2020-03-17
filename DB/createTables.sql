d33b2e6aa0600ea66b715133455b35073433a95c
fábio <fabiouds@hotmail.com>
2020-03-17 23:37:15 +0000

    update

--- DB/createTables.sql
+++ DB/createTables.sql
@@ -6,32 +6,38 @@ CREATE TABLE IF NOT EXISTS Fabrica(
 	nPecasFabrica int
 );
 
-CREATE TABLE IF NOT EXISTS Descarga(
-	tipoDescarga VARCHAR(50) PRIMARY KEY,
-	tipoPecaDescarregada VARCHAR(50),
-    id_fabrica INTEGER NOT NULL,
-	FOREIGN KEY (id_fabrica) REFERENCES Fabrica(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS ZonaDescarga(
+    id SERIAL PRIMARY KEY,
+	tipoDescarga VARCHAR(10),
+	tipoPecaDescarregada VARCHAR(5)
+   
 );
 
+
 CREATE TABLE IF NOT EXISTS Ordem(
-	numeroOrdem VARCHAR(50) PRIMARY KEY,
-	estadoOrdem VARCHAR(50),
+	numeroOrdem VARCHAR(10) PRIMARY KEY,
+	estadoOrdem int,
     pecasProduzidas int,
     pecasProducao int,
     pecasPendentes int,
 	horaEntradaOrdem TIMESTAMP,
 	horaInicioExecucao TIMESTAMP,
 	horaFimExecucao TIMESTAMP,
-    folgaExecucao int,
-    id_fabrica INTEGER NOT NULL,
-    FOREIGN KEY (id_fabrica) REFERENCES Fabrica(id) ON DELETE CASCADE
+    folgaExecucao int
+    );
+
+    CREATE TABLE IF NOT EXISTS Producao(
+    numeroOrdem VARCHAR(10) REFERENCES Ordem(numeroOrdem) PRIMARY KEY,    
+	pecaOrigem VARCHAR(10) ,
+    pecaFinal VARCHAR(10),
+    quantidadeProduzir int,
+    atrasoMaximo int
     );
 
 CREATE TABLE IF NOT EXISTS Maquina(
-	tipoMaquina VARCHAR(50)  PRIMARY KEY,
-    tipoPecaOperada VARCHAR(50),
-    tempoTotal int,
-    id_fabrica INTEGER NOT NULL,
-    FOREIGN KEY (id_fabrica) REFERENCES Fabrica(id) ON DELETE CASCADE
+    numeroOrdem VARCHAR(10) REFERENCES Ordem(numeroOrdem) PRIMARY KEY,    
+    pecaDescarga VARCHAR(10) ,
+    destino VARCHAR(10),
+    quantidadePecasDescarregar int
     );
-
+    
